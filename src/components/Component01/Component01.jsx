8051dcc524c9b68f2c004109ef621a41e3ec9755
johnpetter87 <john.petter.kleppen@gmail.com>
2022-12-18 20:14:28 +0100

    Fjerde

--- src/components/Component01/Component01.jsx
+++ src/components/Component01/Component01.jsx
@@ -1,9 +1,13 @@
 import styles from "./layout.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { faBlackberry } from "@fortawesome/free-brands-svg-icons";
 function Component01() {
   return (
     <div>
       <h3 className={styles.title}>INNER</h3>
+      <p className={styles.para}>TEKST</p>
+      <FontAwesomeIcon className={styles.icon} icon={faBlackberry} />
     </div>
   );
 }
